86dcca73da2020a0378330848ed72b0f0e66a7ab
daik <minichu1103@gmail.com>
2023-05-02 16:32:17 +0700

    init

---
+++ Auth/src/test/java/com/vandai/Auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandai.Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
